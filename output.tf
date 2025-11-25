4258abe24f7c55e69395d3d11c15b47b5aaa328a
Vineeth <113631587+vineethsankre@users.noreply.github.com>
2025-11-25 16:54:34 +0530

    Update output values to reference 'vineeth' resources

--- output.tf
+++ output.tf
@@ -1,16 +1,15 @@
 output "cluster_id" {
-  value = aws_eks_cluster.devopsshack.id
+  value = aws_eks_cluster.vineeth.id
 }
 
 output "node_group_id" {
-  value = aws_eks_node_group.devopsshack.id
+  value = aws_eks_node_group.vineeth.id
 }
 
 output "vpc_id" {
-  value = aws_vpc.devopsshack_vpc.id
+  value = aws_vpc.vineeth_vpc.id
 }
 
 output "subnet_ids" {
-  value = aws_subnet.devopsshack_subnet[*].id
+  value = aws_subnet.vineeth_subnet[*].id
 }
-
